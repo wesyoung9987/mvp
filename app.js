bfb38887c0321ab40ae10fe2bff1d60bfd428d4c
Wes Young <wes.young14@gmail.com>
2016-08-29 23:01:19 -0600

    Built backend

--- app.js
+++ app.js
@@ -1,29 +1,66 @@
-var app = angular.module('photoShare', ['ui.router']);
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-app.factory('posts', [function(){
-  var postsObject = {
-    posts: []
-  }
-  return postsObject;
-}])
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
-  $scope.posts = posts.posts;
+var app = express();
 
-  $scope.addPost = function(){
-    if(!$scope.title || $scope.title === ''){
-      return;
-    }
-    if(!$scope.link || $scope.link === ''){
-      return;
-    }
-    $scope.posts.push({title: $scope.title, link: $scope.link, likes: 0});
-    $scope.title = '';
-    $scope.link = '';
-  };
+var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
 
-  $scope.incrementLikes = function(post){
-    post.likes += 1;
-  };
+mongoose.connect('mongodb://localhost/news');
 
-}])
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
